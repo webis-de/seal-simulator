ee9dccaa39621cfc0c675a2e77a4f7cad16cdd28
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2021-09-09 13:40:46 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
+const { program } = require('commander');
+program.version('0.1.0');
+program
+  .option('-s, --script-directory <directory>', 'TODO description');
+program.parse(process.argv);
+const options = program.opts();
+
+if (options.scriptDirectory) {
+  console.log(`- ${options.scriptDirectory}`);
+}
+
 console.log(`Hello World`);
 
