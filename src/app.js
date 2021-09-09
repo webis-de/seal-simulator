95beeb8c53f7b72d0bf05ee60110929f2710e00f
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2021-09-09 13:45:59 +0200

    update

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ program
   .requiredOption('-s, --script-directory <directory>', 'TODO description')
   .requiredOption('-i, --input-directory <directory>', 'TODO description')
   .requiredOption('-o, --output-directory <directory>', 'TODO description')
-  .option('-p, --port <directory>', 'TODO description');
+  .option('-p, --proxy <address>', 'TODO description');
 program.parse(process.argv);
 const options = program.opts();
 
