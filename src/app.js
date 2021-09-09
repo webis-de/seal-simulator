081bc8fccd7dadc810cf85d5a997d7fe8fc18f61
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2021-09-09 13:43:21 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const { program } = require('commander');
 program.version('0.1.0');
 program
-  .option('-s, --script-directory <directory>', 'TODO description');
+  .requiredOption('-s, --script-directory <directory>', 'TODO description')
+  .requiredOption('-i, --input-directory <directory>', 'TODO description')
+  .requiredOption('-o, --output-directory <directory>', 'TODO description')
+  .option('-p, --script-directory <directory>', 'TODO description');
 program.parse(process.argv);
 const options = program.opts();
 
