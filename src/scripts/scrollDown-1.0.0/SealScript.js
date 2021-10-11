78dc0e2499f8a21688e03cf585f018b92f55e063
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2021-10-12 00:29:45 +0200

    saving snapshots

--- src/scripts/scrollDown-1.0.0/SealScript.js
+++ src/scripts/scrollDown-1.0.0/SealScript.js
@@ -1,16 +1,21 @@
-const { AbstractSealScript } = require("../../AbstractSealScript");
+const fs = require("fs-extra");
 const path = require('path');
 
+const { AbstractSealScript } = require("../../AbstractSealScript");
 const seal = require('../../seal');
 
 const NAME = "ScrollDown";
 const VERSION = "1.0.0";
 
 const SCRIPT_OPTION_URL = "url";
+const SCRIPT_OPTION_MIN_HEIGHT = "minHeigth";
+const DEFAULT_MIN_HEIGHT = 663;
 
 exports.SealScript = class extends AbstractSealScript {
 
   #scriptOptions;
+  #url;
+  #minHeight;
 
   constructor(scriptDirectory, inputDirectory) {
     super(NAME, VERSION, scriptDirectory, inputDirectory);
@@ -20,6 +25,13 @@ exports.SealScript = class extends AbstractSealScript {
       throw new Error("This script requires an input directory that contains "
         + "at least a '" + seal.DEFAULT_SCRIPT_CONFIGURATION_FILE + "' with an '"
         + SCRIPT_OPTION_URL + "' attribute.");
+    } else {
+      this.#url = this.#scriptOptions[SCRIPT_OPTION_URL];
+    }
+    if (this.#scriptOptions[SCRIPT_OPTION_MIN_HEIGHT] === undefined) {
+      this.#minHeight = DEFAULT_MIN_HEIGHT;
+    } else {
+      this.#minHeight = this.#scriptOptions[SCRIPT_OPTION_MIN_HEIGHT];
     }
     seal.log("script-options-complete", this.#scriptOptions)
   }
@@ -28,12 +40,28 @@ exports.SealScript = class extends AbstractSealScript {
     const browserContext = browserContexts[seal.DEFAULT_BROWSER_CONTEXT];
 
     const page = await browserContext.newPage();
-    await page.goto(this.#scriptOptions[SCRIPT_OPTION_URL]);
+    await page.goto(this.#url);
+
+    await page.waitForLoadState('domcontentloaded');
+    await page.waitForLoadState('networkidle');
+
+    const viewportSize = page.viewportSize();
+    viewportSize.height = await seal.getScrollHeight(page);
+    await page.setViewportSize(viewportSize); 
+
+    const nodesSnapshot = seal.getNodesSnapshot(page)
+      .then(nodes => fs.writeFile(path.join(outputDirectory, "nodes.jsonl"), nodes));
+
     await page.screenshot({
       path: path.join(outputDirectory, "screenshot.png"),
-      fullPage: true
+      fullPage: true 
     });
 
+    const domSnapshot = seal.getDomSnapshot(page)
+      .then(dom => fs.writeFile(path.join(outputDirectory, "dom.html"), dom));
+
+    await nodesSnapshot;
+    await domSnapshot;
     const simulationComplete = true;
     return simulationComplete;
   }
--- src/seal.js
+++ src/seal.js
@@ -36,3 +36,101 @@ const log = function(name, attributes = {}) {
 }
 exports.log = log;
 
+const getScrollHeight = async function(page) {
+  return page.evaluate(() => document.body.scrollHeight);
+}
+exports.getScrollHeight = getScrollHeight;
+
+const getScrollYPosition = async function(page) {
+  return page.evaluate(() => window.pageYOffset + window.innerHeight);
+}
+exports.getScrollYPosition = getScrollYPosition;
+
+const isScrolledToBottom = async function(page) {
+  return page.evaluate(() => (window.pageYOffset + window.innerHeight) >= document.body.scrollHeight);
+}
+exports.isScrolledToBottom = isScrolledToBottom;
+
+const getDomSnapshot = async function(page) {
+  return page.content();
+}
+exports.getDomSnapshot = getDomSnapshot;
+
+const getNodesSnapshot = async function(page, includeCss = true) {
+  const nodesSnapshot = await page.evaluate((includeCss) => {
+    const nodes = [];
+
+    const getNodeCss = (element) => {
+      const styles = window.getComputedStyle(element);
+      const cssArray = Array.from(styles).map(name => [name, styles.getPropertyValue(name)]);
+      const css = {};
+      cssArray.forEach((data) => { css[data[0]] = data[1] });
+      return css;
+    };
+
+    const isVisible = (element) => {
+      // https://stackoverflow.com/a/33456469
+      return !!( element.offsetWidth || element.offsetHeight || element.getClientRects().length );
+    }
+
+    const traverse = (node, parentPath, nodeName, nodeNameNumber) => {
+      const nodePath = parentPath + "/" + nodeName + "[" + nodeNameNumber + "]";
+      const nodeId = node.id ? node.id : undefined;
+      let nodeVisible, nodeClasses, nodeText, nodeBox, nodeCss;
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        nodeClasses = Array.from(node.classList);
+        nodeText = node.innerText;
+        if (nodeText) { nodeText = nodeText.trim().replace(/\n/g, "\\n"); }
+        if (nodeText === "") {nodeText = undefined; }
+        nodeVisible = isVisible(node);
+        nodeBox = node.getBoundingClientRect();
+        if (includeCss) { nodeCss = getNodeCss(node); }
+      } else {
+        nodeText = node.textContent.trim().replace(/\n/g, "\\n");
+        if (nodeText === "") { return }
+        nodeVisible = isVisible(node.parentElement);
+        const nodeRange = document.createRange();
+        nodeRange.selectNode(node);
+        nodeBox = nodeRange.getBoundingClientRect();
+      }
+      const xmin = nodeBox.left;
+      const ymin = nodeBox.top;
+      const xmax = xmin + nodeBox.width;
+      const ymax = ymin + nodeBox.height;
+      nodes.push(JSON.stringify({
+        xPath: nodePath,
+        visible: nodeVisible,
+        id: nodeId,
+        classes: nodeClasses,
+        position: [xmin, ymin, xmax, ymax],
+        text: nodeText,
+        css: nodeCss
+      }));
+
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        const counts = {};
+        const children = node.childNodes;
+        
+        for (let c = 0; c < children.length; ++c) {
+          const child = children[c];
+          if (child.nodeType === Node.ELEMENT_NODE || child.nodeType === Node.TEXT_NODE) {
+            const childName = child.nodeType === Node.TEXT_NODE ? "text()" : child.tagName;
+
+            if (typeof counts[childName] === "undefined") {
+              counts[childName] = 1;
+            } else {
+              counts[childName] += 1;
+            }
+            traverse(child, nodePath, childName, counts[childName]);
+          }
+        }
+      }
+    };
+
+    traverse(document.getElementsByTagName("body")[0], "/HTML[1]", "BODY", 1)
+    return nodes.join("\n");
+  }, includeCss);
+  return nodesSnapshot;
+}
+exports.getNodesSnapshot = getNodesSnapshot;
+
